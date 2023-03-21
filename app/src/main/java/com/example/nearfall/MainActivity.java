c2b1d8020cdf89d28548abd918314002fe69f662
cede <ede003@gmail.com>
2023-03-21 12:50:44 -0600

    Main Activity to turn detection off on app start

--- app/src/main/java/com/example/nearfall/MainActivity.java
+++ app/src/main/java/com/example/nearfall/MainActivity.java
@@ -14,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
         //Set view to activity_main.xml
         setContentView(R.layout.activity_main);
         userManager = new UserManager(MainActivity.this);
+        // Turns off detection to start
+        //TODO: May be easier to remove from database and just init with a variable here instead
+        userManager.setDetection("Off");
+    }
+
+    @Override
+    public void onBackPressed() {
+        // do nothing
+        // disable back button
     }
 
     public static UserManager getUserManager() {
